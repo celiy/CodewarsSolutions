693bd0d1f7611c5614a55017a21af0759b0c2365
Diogo Carvalho Viegas <sasbrazilian@gmail.com>
2024-08-09 21:57:51 -0300

    Create Rot13.py

---
+++ 5 kyu/Rot13.py
@@ -0,0 +1,36 @@
+#ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+#Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+#Please note that using encode is considered cheating.
+
+def rot13(message):
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    char = string = ""
+    x = 0
+    for c in message:
+        if c in alph.lower():
+            x = alph.find(c)+1
+            for n in range(0,13):
+                if x in range(len(alph)):
+                    char = alph[x]
+                else:
+                    x=0
+                    char = alph[x]
+                x+=1
+            string += char
+            x = 0
+        elif c in alph.upper():
+            x = alph.upper().find(c)+1
+            for n in range(0,13):
+                if x in range(len(alph)):
+                    char = alph[x].upper()
+                else:
+                    x=0
+                    char = alph[x].upper()
+                x+=1
+            string += char
+            x = 0
+        else:
+            string += c
+    return string
